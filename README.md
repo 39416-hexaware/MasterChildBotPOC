5270e38ddec3007c709f2855493a8c1f0548f548
Mubashshir Mohammed <39416.hexaware@gmail.com>
2018-03-06 11:03:26 +0530

    Initial Draft

---
+++ README.md
@@ -0,0 +1,3 @@
+# Master Chid Bot Communicator
+
+[![N|Solid](https://cldup.com/dTxpPi9lDf.thumb.png)](https://www.google.com)
---
+++ index.js
@@ -0,0 +1,29 @@
+//imports
+var express = require('express');
+var bodyParser = require('body-parser');
+var requestAPI = require('request');
+
+app = express();
+//Create express object
+
+var port = process.env.PORT || 7001;
+//Assign port
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//Configuring express app behaviour
+
+app.get("/api", function (req, res) {
+    res.send("Server running!");
+  });
+  //GET Endpoint
+
+  app.post("/api", function (req, res) {
+    console.log(req);
+    res.json('Testing');
+  });
+  //POST EndPoint
+
+  console.log("Server Running at Port : " + port);
+  
+  app.listen(port);
